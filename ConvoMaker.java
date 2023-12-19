5a4b93e4b5c9cfcd6dc525b4aa3de6dc8ead1bd2
Ranjith <143414554+iamranjith04@users.noreply.github.com>
2023-12-19 21:57:30 +0530

    Add files via upload

---
+++ ConvoMaker.java
@@ -0,0 +1,105 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+class ConvoMaker extends Frame implements ActionListener
+{
+	Button X,SEND;
+	TextField T1,T2;
+	Label L1,L2,title;
+	String state="";
+	Socket myserver;
+	Socket client;
+	ServerSocket ss;
+	int run=1;
+	ConvoMaker(){
+		setTitle("ConvoMaker");
+		setBackground(Color.GREEN);
+		title=new Label("ConvoMaker");
+		title.setFont(new Font("Arial", Font.PLAIN, 30));
+		title.setBounds(600,50,500,50);
+		add(title);
+		X=new Button("X");
+		SEND=new Button("SEND");
+		T1=new TextField(0);
+		T1.setFont(new Font("Arial", Font.PLAIN, 18));
+		T2=new TextField(0);
+		T2.setFont(new Font("Arial", Font.PLAIN, 18));
+		L1=new Label("Type:");
+		L2=new Label("Received:");
+		X.setBounds(1300,40,20,20);
+		X.setBackground(Color.RED);
+		add(X);
+		X.addActionListener(this);
+		T1.setBounds(800,500,400,100);
+		add(T1);
+		T2.setBounds(300,200,400,100);
+		add(T2);
+		L1.setBounds(750,500,40,20);
+		add(L1);
+		L2.setBounds(200,200,60,20);
+		add(L2);
+		SEND.setBounds(1200,500,40,20);
+		add(SEND);
+		SEND.setBackground(Color.CYAN);
+		SEND.addActionListener(this);	
+		setSize(800,700);
+		setLayout(null);
+		setVisible(true);
+		try{
+				ss= new ServerSocket(4444);
+            			ss.setSoTimeout(10000);
+			}
+		catch (IOException ex) {
+                		ex.printStackTrace();
+           		}
+		
+		startServer();
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==X){
+			System.exit(0);
+		}
+		if(e.getSource()==SEND){
+			try{
+				client=new Socket("localhost",5555);
+				DataOutputStream dout=new DataOutputStream(client.getOutputStream());
+				String mesg=T1.getText();
+				dout.writeUTF(mesg);
+				client.close();
+			}
+			catch (IOException ex) {
+                		ex.printStackTrace();
+           		}
+		}
+		
+		
+	}
+
+	void startServer() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket server = ss.accept();
+                    DataInputStream din = new DataInputStream(server.getInputStream());
+                    String mesg = din.readUTF();
+                    T2.setText(mesg); 
+                    din.close();
+                    server.close();
+                } catch (SocketTimeoutException ex) {
+                    System.out.println("Timeout occurred. No connections within the specified time.");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+    }
+	public static void main(String[] argu)
+	{
+		
+		new ConvoMaker();
+	}
+}
